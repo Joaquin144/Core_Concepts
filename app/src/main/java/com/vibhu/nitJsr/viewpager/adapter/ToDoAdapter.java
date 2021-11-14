130223664023085a42358441cd6ec544d6d2b6fc
Joaquin144 <vvpp898.comco@gmail.com>
2021-11-15 03:58:04 +0530

    Delete option also added in todo list.

--- app/src/main/java/com/vibhu/nitJsr/viewpager/adapter/ToDoAdapter.java
+++ app/src/main/java/com/vibhu/nitJsr/viewpager/adapter/ToDoAdapter.java
@@ -1,13 +1,24 @@
 package com.vibhu.nitJsr.viewpager.adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.vibhu.nitJsr.viewpager.R;
 import com.vibhu.nitJsr.viewpager.models.ToDOModel;
 
@@ -39,13 +50,46 @@ public class ToDoAdapter extends RecyclerView.Adapter<ToDoAdapter.ViewHolder> {
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         private TextView t1;
+        private ImageView delete;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             t1=itemView.findViewById(R.id.todoTV);
+            delete=itemView.findViewById(R.id.toDoIV);
         }
 
         public void setData(int position) {
-            t1.setText(list.get(position).getTask());
+            String str=list.get(position).getTask();
+            t1.setText(str);
+            delete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    FirebaseFirestore firestore=FirebaseFirestore.getInstance();
+                    firestore.collection("tasks").whereEqualTo("name",str).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                            if(task.isSuccessful()){
+                                for(QueryDocumentSnapshot document : task.getResult()){
+                                    firestore.collection("tasks").document(document.getId()).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Toast.makeText(itemView.getContext(), "Success", Toast.LENGTH_SHORT).show();
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(itemView.getContext(), "Failed to delete", Toast.LENGTH_SHORT).show();
+                                        }
+                                    });
+                                }
+                            }
+                            else{
+                                Log.d("####","Ooops deleteing task failed to get said name task");
+                            }
+                        }
+                    });
+                }
+            });
+
         }
     }
 }
